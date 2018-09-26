5d515ae6a649a209b1f9babafa2bc198377a21a2
Nicholas Mendez <snickdx@gmail.com>
2018-09-26 10:35:35 -0400

    second commit

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const readline = require('readline');
 const Bull = require('./bull');
+var fs = require('fs');
+var path = process.cwd();
 
 function newReader() {
     return readline.createInterface({
@@ -27,16 +29,22 @@ function play(count, game) {
 }
 
 function start() {
-    let r1 = newReader();
-    r1.question('Player 1 number: ', (num1) => {
-        r1.close();
-        let r2 = newReader();
-        r2.question('Player 2 number: ', (num2) => {
-            r2.close();
-            play(0, new Bull(num1, num2));
-        });
+    // let r1 = newReader();
+    // r1.question('Player 1 number: ', (num1) => {
+    //     r1.close();
+    //     console.clear();
+    //     let r2 = newReader();
+    //     r2.question('Player 2 number: ', (num2) => {
+    //         r2.close();
+    //         console.clear();
+    //         play(0, new Bull(num1, num2));
+    //     });
 
-    });
+    // });
+
+    let num1 = (fs.readFileSync(path + "\\p1num.txt")).toString();
+    let num2 = (fs.readFileSync(path + "\\p2num.txt")).toString();
+    play(0, new Bull(num1, num2));
 
 }
 
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Bull Game
+
+## Game Rules
+* Players each think of a secret 4 digit number with no repeated digits.
+* Players takes turns gussing the other players number.
+* After a guess is made the game indicates how many digits where from the guess are correct but in the wrong position (cows) and how many digits are correct and in the right position (bulls). 
+* First player to get 4 bulls first wins.
+
+## Instructions
+Players must input there secret number in p1num.txt and p2num.txt.
+Run the following line to start the game:
+```
+$ node main.js
+```
