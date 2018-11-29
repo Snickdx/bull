2947b5ac7a6c31dc9c1aefd1acfb49fa77c6a717
Nicholas Mendez <snickdx@gmail.com>
2018-11-29 20:32:44 +0000

    added material

--- test.js
+++ test.js
@@ -1,53 +1,19 @@
-function promiseTimeout(cb, timeout) {
-  return new Promise((resolve => {
+function syncSetTimeout(cb, timeout) {
+  return new Promise(resolve => {
     setTimeout(() => {
-      cb();
-      resolve('resolved');
+      resolve(cb()||"resolved");//cb() called after timeout
     }, timeout);
   });
 }
 
-
 (async ()=>{
-    // let a = 0;
-    // setTimeout(()=>{ console.log(a)}, 500);
-    // a=11;
-    // setTimeout(()=>{ console.log(a)}, 500)
-    let a = 0;
-    await promiseTimeout(()=>{ console.log(a)}, 500);
-    a=11;
-    await promiseTimeout(()=>{ console.log(a)}, 500)
+  
+
+    await syncSetTimeout(()=>{ console.log("1s")}, 1000);
+   
+    await syncSetTimeout(()=>{ console.log("2s")}, 1000)
     
-    await messagePromiseMaker(mailOptions1);
-    await messagePromiseMaker(mailOptions2);
   
 })()
 
-
-
-let transporter = nodemailer.createTransport({
-    sendmail: true,
-    newline: 'unix',
-    path: '/usr/sbin/sendmail'
-});
-
-
-function messagePromiseMaker(mailOptions){
-    return new Promise((resolve, reject)=>{
-        transporter.sendMail(mailOptions, (err, info) => {
-            if(err)reject("error");
-            console.log(info.envelope);
-            console.log(info.messageId);
-            resolve(info.messageId);
-        });
-    });
-}
-
-
-let promArr = [];
-mailOptions.forEach(ele=>{
-    promArr.push(ele);
-})
-
-Promise.all(promArr).then(()=>console.log("done"));
 
